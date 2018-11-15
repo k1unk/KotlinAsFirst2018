286349144aeb94580d44d31a2c2e6bacb267d0c0
K1unk <klunk@inbox.ru>
2018-11-15 20:59:24 +0300

    Urok 5, 6

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -220,6 +220,7 @@ fun plusMinus(expression: String): Int {
                 throw java.lang.IllegalArgumentException()
         }
     }
+    if (expression == "") { throw java.lang.IllegalArgumentException() }
 
     if (parts.size % 2 == 0) { throw java.lang.IllegalArgumentException() }
     else {
